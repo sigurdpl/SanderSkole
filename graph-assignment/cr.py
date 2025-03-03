a2c8d5f4e08c43e6acbe0287e4f513004bbdee3c
Sander <sander@gmail.com>
2025-03-03 17:26:51 +0100

    fjerna den andre funksjonen

--- graph-assignment/cr.py
+++ graph-assignment/cr.py
@@ -42,7 +42,7 @@ class graphInfo():
         self.current_colour = 0 
 
 
-def colour_refinement():
+def basic_colorref(graphlist):
     with open(benchmarklist2[4]) as f:
         L = load_graph(f, read_list=True)
 
@@ -71,7 +71,6 @@ def colour_refinement():
         results.append((cf, itr, discrete))
         graph_colour_freq.append(freq_thing)
 
-    # print(results)
 
     equiv_dict = {}
     for i in range(len(L[0])):
@@ -80,16 +79,18 @@ def colour_refinement():
             equiv_dict[equiv_key] = []
         equiv_dict[equiv_key].append(i)
 
-    print("here is the equiv dict")
-    print(equiv_dict.values())
+    # print("here is the equiv dict")
+    # print(equiv_dict.values())
 
     final_output = []
     for i in equiv_dict.values():
         temp_final_output = tuple([i, list(colour_freq_list[i[0]]), itr_list[i[0]], discrete_list[i[0]]])
         final_output.append(temp_final_output)
 
-    print("here is the final output?")
-    print(final_output)
+    # print("here is the final output?")
+    # print(final_output)
+
+    return final_output
 
 def cr(graph_num, graphInf:graphInfo):
     # with open(benchmarklist[1]) as f:
@@ -149,7 +150,7 @@ def cr(graph_num, graphInf:graphInfo):
             current_colouring[vert] = graphInf.colour_mapping[neighbours_list_tuple] # {1: (1,1)]}
 
         if previous_colouring == current_colouring or stable:
-            print(f"\nStable colouring reached after {itr_count} iterations")
+            # print(f"\nStable colouring reached after {itr_count} iterations")
             frequency_counter = Counter(Counter(current_colouring.values()).values())
             freq_thing = Counter(current_colouring.values())
             # print(f"what have i counted?{freq_thing}")
@@ -169,104 +170,6 @@ def cr(graph_num, graphInf:graphInfo):
         itr_count += 1
         # print(graphInf.current_colour)
 
-# print(cr())
-# graphlist = ['crSamples/test_3reg.grl', 'crSamples/colorref_smallexample_4_7.grl', 'crSamples/colorref_smallexample_6_15.grl', 'crSamples/test_iter.grl', 'crSamples/colorref_largeexample_6_960.grl']
-colour_refinement()
+# colour_refinement()
 
-def new_colour_refinement():
-    with open(graphlist[2]) as f:
-        L = load_graph(f, read_list=True)
-
-    graphs = L[0]
-    
-    # init dicts and initialise colouring of all verticies to the colour 1.
-    previous_colouring = [{vertex.label: 1 for vertex in G.vertices} for G in graphs]
-    current_colouring = [{vertex.label: 1 for vertex in G.vertices} for G in graphs]
-    
-    glob_colour_map = {}
-    current_colour = 1
-
-    # step 2 init iteration count to 0
-    stable_graphs = [False] * len(graphs)
-    stable_dict = {}
-    itr_count = 0
-
-    while True:
-        # go through all the verticies in the graphs and create lists of their neighbours, the colouring of those neighbours, the multiset for a graph G and a list of all colouring multisets
-        colouring_multisets = []
-        for G, colour in zip(graphs, previous_colouring):
-            if stable_graphs[graphs.index(G)]:
-                print(f"skipping graph: {graphs.index(G)}")
-                colouring_multisets.append(None)
-                continue
-            multiset = {}
-            for vertex in G.vertices:
-                neighbours = [incident.other_end(vertex).label for incident in vertex.incidence]
-                nh_colouring = [colour[x] for x in neighbours]
-                multiset[vertex.label] = tuple(sorted(nh_colouring))
-            colouring_multisets.append(multiset)
-
-        colour_mapping = {}
-        # current_colour = 0
- 
-        # create the colour mapping by making the neighbourhood structure
-        # the key of a dict, and the colour of that neighbourhood structure the value
-        for G, multiset in zip(graphs, colouring_multisets):
-            if stable_graphs[graphs.index(G)]:
-                continue
-            for nh_tuple in multiset.values():
-                if nh_tuple not in glob_colour_map:
-                    current_colour += 1
-                    glob_colour_map[nh_tuple] = current_colour
-                colour_mapping[nh_tuple] = glob_colour_map[nh_tuple]
-
-        # assign each vertex to the neihbourhood(based on colouring) structure it belongs to
-        for i in range(len(graphs)):
-            if stable_graphs[i]:
-                continue
-            for vertex in graphs[i].vertices:
-                nh_tuple = colouring_multisets[i][vertex.label]
-                current_colouring[i][vertex.label] = colour_mapping[nh_tuple]
-
-        # check if all graphs remain the same from previous iteration, if they are not all the same we continue iterating
-        stable_colouring = True
-        for i, both in enumerate(zip(previous_colouring, current_colouring)):
-            if stable_graphs[i]:
-                continue
-            if both[0] != both[1]:
-                stable_colouring = False
-            if both[0] == both[1]:
-                stable_dict[i] = itr_count
-                stable_graphs[i] = True
-
-        if stable_colouring or itr_count == 8:
-            print(glob_colour_map)
-            break
-
-        previous_colouring = [current.copy() for current in current_colouring]
-        print(f"iteration: {itr_count} stable graphs: {[(i, stable_graphs[i]) for i in range(len(stable_graphs))]}")
-        itr_count += 1
-
-    results = []
-    
-    for i in range(len(graphs)):
-        frequency_counter = Counter(Counter(current_colouring[i].values()).values())
-        isDiscrete = list(frequency_counter.values())[0] == len(graphs[i].vertices)
-        results.append((dict(frequency_counter), itr_count, isDiscrete))
-
-    # this for loop does not work yet will look into later (does not take into account the thing)
-    equiv_classes = defaultdict(list)
-    for i in range(len(results)):
-        equiv_class_tuple = (tuple(results[0]), results[1], results[2])
-        # print(type(equiv_class_tuple))
-        # equiv_classes[equiv_class_tuple].append(i)
-
-    
-    return results, stable_dict, equiv_classes
-
-
-# results, termination, equiv = new_colour_refinement()
-# print(results)
-# print(termination)
-# print(equiv)
 
